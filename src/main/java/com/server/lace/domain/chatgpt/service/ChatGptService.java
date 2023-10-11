061798934c83122281a63e7a39004c10e42ede12
재명 <ljm9894@dgsw.hs.kr>
2023-10-12 00:57:05 +0900

    feat :: ChatGPT service

---
+++ src/main/java/com/server/lace/domain/chatgpt/service/ChatGptService.java
@@ -0,0 +1,70 @@
+package com.server.lace.domain.chatgpt.service;
+
+import com.server.lace.domain.chatgpt.presentation.dto.request.ChatGptMessage;
+import com.server.lace.domain.chatgpt.presentation.dto.request.ChatGptRequest;
+import com.server.lace.domain.chatgpt.presentation.dto.request.QuestionRequest;
+import com.server.lace.domain.chatgpt.presentation.dto.response.ChatGptResponse;
+import com.server.lace.global.config.ChatGptConfig;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class ChatGptService {
+    private final RestTemplate restTemplate;
+
+
+    @Value("${api-key.chat-gpt}")
+    private String apiKey;
+
+    public HttpEntity<ChatGptRequest> buildHttpEntity(ChatGptRequest chatGptRequest){
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType((MediaType.parseMediaType(ChatGptConfig.MEDIA_TYPE)));
+        httpHeaders.add(ChatGptConfig.AUTHORIZATION, ChatGptConfig.BEARER + apiKey);
+        return new HttpEntity<>(chatGptRequest, httpHeaders);
+    }
+    public ChatGptResponse getResponse(HttpEntity<ChatGptRequest> chatGptRequestHttpEntity) {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(60000);
+        requestFactory.setReadTimeout(60 * 1000);
+        restTemplate.setRequestFactory(requestFactory);
+
+        ResponseEntity<ChatGptResponse> responseEntity = restTemplate.postForEntity(
+                ChatGptConfig.CHAT_URL,
+                chatGptRequestHttpEntity,
+                ChatGptResponse.class);
+        return responseEntity.getBody();
+    };
+    public ChatGptResponse askQuestion(QuestionRequest questionRequest){
+        List<ChatGptMessage> messages = new ArrayList<>();
+        messages.add(ChatGptMessage.builder()
+                .role(ChatGptConfig.ROLE)
+                .content(questionRequest.getQuestion())
+                .build());
+        return this.getResponse(
+                this.buildHttpEntity(
+                        new ChatGptRequest(
+                                ChatGptConfig.CHAT_MODEL,
+                                ChatGptConfig.MAX_TOKEN,
+                                ChatGptConfig.TEMPERATURE,
+                                ChatGptConfig.STREAM,
+                                messages
+                        )
+                )
+        );
+    }
+
+}
