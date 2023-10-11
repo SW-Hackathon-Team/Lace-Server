597626a53532f5e587e78e22994a63534f18dfce
KimGyeongsuuu <sk62628504@gmail.com>
2023-10-11 16:58:35 +0900

    add :: jwt 설정

---
+++ src/main/java/com/server/lace/global/security/jwt/TokenProvider.java
@@ -0,0 +1,93 @@
+package com.server.lace.global.security.jwt;
+
+import com.server.lace.global.security.exception.TokenExpirationException;
+import com.server.lace.global.security.exception.TokenNotValidException;
+import com.server.lace.global.security.jwt.properties.JwtProperties;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.time.ZonedDateTime;
+import java.util.Date;
+
+@Getter
+@Component
+@RequiredArgsConstructor
+public class TokenProvider {
+
+    private final JwtProperties jwtProperties;
+    private final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 120;
+    private final long REFRESH_TOKEN_EXPIRE_TIME = ACCESS_TOKEN_EXPIRE_TIME * 12 * 7;
+
+    @AllArgsConstructor
+    private enum TokenType {
+        ACCESS_TOKEN("accessToken"),
+        REFRESH_TOKEN("refreshToken");
+        String value;
+    }
+
+    @AllArgsConstructor
+    private enum TokenClaimName {
+        USER_ID("id"),
+        TOKEN_TYPE("tokenType");
+        String value;
+    }
+
+    private Key getSignInKey(String secretKey) {
+        byte[] bytes =secretKey.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(bytes);
+    }
+
+    private String generateToken(String userId, TokenType tokenType, String secret, long expireTime) {
+        final Claims claims = Jwts.claims();
+        claims.put(TokenClaimName.USER_ID.value, userId);
+        claims.put(TokenClaimName.TOKEN_TYPE.value, tokenType);
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+expireTime))
+                .signWith(getSignInKey(secret), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public Claims extractAllClaims(String token, String secret) {
+        token = token.replace("Bearer ", "");
+        try {
+            return Jwts.parserBuilder()
+                    .setSigningKey(getSignInKey(secret))
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (ExpiredJwtException e) {
+            throw new TokenExpirationException();
+        } catch (JwtException e) {
+            throw new TokenNotValidException();
+        }
+    }
+
+    public ZonedDateTime getExpiredAtToken() {
+        return ZonedDateTime.now().plusSeconds(ACCESS_TOKEN_EXPIRE_TIME);
+    }
+
+    public String getUserId(String token, String secret) {
+        return extractAllClaims(token, secret).get(TokenClaimName.USER_ID.value, String.class);
+    }
+
+    public String getTokenType(String token, String secret) {
+        return extractAllClaims(token, secret).get(TokenClaimName.TOKEN_TYPE.value, String.class);
+    }
+
+    public String generatedAccessToken(String email) {
+        return generateToken(email, TokenType.ACCESS_TOKEN, jwtProperties.getAccessSecret(), ACCESS_TOKEN_EXPIRE_TIME);
+    }
+
+    public String generatedRefreshToken(String email) {
+        return generateToken(email, TokenType.REFRESH_TOKEN, jwtProperties.getRefreshSecret(), REFRESH_TOKEN_EXPIRE_TIME);
+    }
+
+}
