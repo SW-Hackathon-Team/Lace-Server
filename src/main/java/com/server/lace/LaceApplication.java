1b2fcea7ebd373d3ed62cdb8d67785e9151bf216
KimGyeongsuuu <sk62628504@gmail.com>
2023-10-11 17:02:59 +0900

    add :: 어노테이션 추가

--- src/main/java/com/server/lace/LaceApplication.java
+++ src/main/java/com/server/lace/LaceApplication.java
@@ -2,8 +2,12 @@ package com.server.lace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableConfigurationProperties
 public class LaceApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/server/lace/global/config/EnableConfigurationPropertiesConfig.java
+++
@@ -1,9 +0,0 @@
-package com.server.lace.global.config;
-
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableConfigurationProperties
-public class EnableConfigurationPropertiesConfig {
-}
