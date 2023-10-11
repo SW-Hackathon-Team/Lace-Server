95e442d950baab0b20e54ca23aca857d223c6839
KimGyeongsuuu <sk62628504@gmail.com>
2023-10-11 17:01:49 +0900

    add :: config 설정

---
+++ src/main/java/com/server/lace/global/config/ConfigurationPropertiesScanConfig.java
@@ -0,0 +1,10 @@
+package com.server.lace.global.config;
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationPropertiesScan
+public class ConfigurationPropertiesScanConfig{
+
+}
