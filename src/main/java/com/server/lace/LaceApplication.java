ad33696855f3e68c40600ab9b5b52ebc6b89c33c
jyk1029 <jyk102999@gmail.com>
2023-10-11 15:11:01 +0900

    프로젝트 세팅

---
+++ src/main/java/com/server/lace/LaceApplication.java
@@ -0,0 +1,13 @@
+package com.server.lace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/server/lace/LaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.lace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
