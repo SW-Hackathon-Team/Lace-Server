38feb6f10a707fb09875ed64b6d65dadbfc2f264
KimGyeongsuuu <sk62628504@gmail.com>
2023-10-11 16:58:15 +0900

    add :: handler 추가

---
+++ src/main/java/com/server/lace/global/security/handler/CustomAuthenticationEntryPointHandler.java
@@ -0,0 +1,23 @@
+package com.server.lace.global.security.handler;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Log4j2
+public class CustomAuthenticationEntryPointHandler implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        log.info("=== AuthenticationEntryPoint ===");
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED); // 401
+    }
+
+}
